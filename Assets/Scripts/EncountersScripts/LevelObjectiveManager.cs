abd0ae98882bdf8ae7cd3fa0be0c023e43b245a9
unknown <pkl9pkl@gmail.com>
2023-06-20 18:31:08 +0200

    merge

---
+++ Assets/Scripts/EncountersScripts/LevelObjectiveManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelObjectiveManager : MonoBehaviour
+{
+    List<LevelObjective> levelObjectives;
+    int objectivesCount;
+
+    bool alreadyCompleted = false;
+
+    void Awake()
+    {
+        levelObjectives = new List<LevelObjective>(FindObjectsOfType<LevelObjective>());
+        objectivesCount = levelObjectives.Count;
+
+        if (objectivesCount <= 0)
+        {
+            OnAllObjectivesCompleted();
+        }
+    }
+
+    public void OnObjectiveCompleted()
+    {
+        objectivesCount -= 1;
+        if (objectivesCount <= 0 && !alreadyCompleted)
+        {
+            OnAllObjectivesCompleted();
+        }
+    }
+
+    void OnAllObjectivesCompleted()
+    {
+        alreadyCompleted = true;
+        SceneManager.LoadScene("EncounterReward");
+    }
+}
